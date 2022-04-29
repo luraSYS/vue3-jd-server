30d44886a5366ad0cbf646df13f13834c3426add
lura_syxilia <1329317082a@gmail.com>
2022-04-29 16:44:19 +0800

    提交配置信息

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // jwt部分配置
+  secretKey: '*^*$alren*^*',
+  survivalTime: '10h',
+  // 数据库相关配置
+  host: '127.0.0.1',
+  user: 'hb_test',
+  password: '111',
+  database: 'shoppingplatform',
+}
